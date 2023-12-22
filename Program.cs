d4431719f355cd4b1bae011c11d26952e62d6dad
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-22 22:00:41 +0400

    Array, array1,3 created

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int[] Array = new int[5];
+Array[0] = 5;
+Array[1] = 7;
+Array[2] = 5;
+Array[3] = 4;
+Array[4] = 3;
+Console.WriteLine(Array[0]);
+Console.WriteLine(Array[1]);
+Console.WriteLine(Array[2]);
+Console.WriteLine(Array[3]);
+Console.WriteLine(Array[4]);
+
+int[] array1 = new int[5]{1,2,3,4,5};
+int[] array3 = {1,2,3,4,5};
