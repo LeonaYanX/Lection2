a0b33a9e849480d647a93eddd509ba6c1ec0f692
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-22 22:39:39 +0400

    Task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter the number of elements");
+int N = int.Parse(Console.ReadLine());
+int[] array = new int[N];
+int i = 0;
+while(i<array.Length)
+{
+    array[i]=i+1;
+    Console.Write($"{array[i]}\t");
+    i= i+1;
+}
