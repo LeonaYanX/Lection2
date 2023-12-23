8de70dff0bb78731de5e5d33d8af10ce716be870
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-23 15:22:00 +0400

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+          int[] array = new int[10];
+   
+   for (int i = 0; i < array.Length; i++)
+   {
+       Console.WriteLine($"Enter the element {i}");
+       array[i] = int.Parse(Console.ReadLine());
+   }
+   for (int j = 0; j < array.Length; j++)
+   {
+    if(array[j]%2==0)
+    {
+        Console.Write(array[j] + "\t");
+    }
+   }
+   
+    }
+}
