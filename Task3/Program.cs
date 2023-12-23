6cb9f2aa0b09f76148bfc89efd7cf60af049e63b
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-23 15:42:10 +0400

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter the number of weigths");
+int N = int.Parse(Console.ReadLine());
+int[] weigths = new int [N];
+for(int i = 1; i < weigths.Length ; i++)
+{
+    Console.WriteLine($"Enter the wegth {i}");
+    weigths[i] = int.Parse(Console.ReadLine());
+}
+int maxWeigth = weigths[0];
+for(int j = 1; j<weigths.Length ; j++)
+{
+    if(maxWeigth<weigths[j])
+    {
+        maxWeigth = weigths[j];
+    }
+}
+Console.WriteLine(maxWeigth);
